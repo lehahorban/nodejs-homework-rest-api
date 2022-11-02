9c4bfff411ea69e8a2f2a2cfd0959b3a06ecf477
Oleksii Horban <lehagorban93@gmail.com>
2022-11-02 20:45:15 +0200

    commit

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -9,9 +9,12 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
 const updateAvatar = async (req, res) => {
     const { _id } = req.user;    
     const { path: tempUpload, originalname } = req.file
-    const resultUpload = path.join(avatarsDir, originalname)   
+    const extension = originalname.split(".").pop()
+    const filename = `${_id}.${extension}`
+    const resultUpload = path.join(avatarsDir, filename)    
     await fs.rename(tempUpload, resultUpload)
-    const avatarURL = path.join("avatars", originalname)
+    const avatarURL = path.join("avatars", filename)
+   
     const result = await User.findByIdAndUpdate(_id, { avatarURL })
      if (!result) {
         throw RequestError(401, "Not authorized")
