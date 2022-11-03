400af3b3cf92d583bea14f4c4393ac3d1fb2d738
Oleksii Horban <lehagorban93@gmail.com>
2022-11-03 15:20:32 +0200

    commit

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,7 +1,8 @@
 const fs = require("fs/promises")
 const path = require("path")
 const { User } = require("../../models/user")
-const {RequestError} = require("../../helpers")
+const { RequestError } = require("../../helpers")
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars")
 
@@ -11,6 +12,10 @@ const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file
     const extension = originalname.split(".").pop()
     const filename = `${_id}.${extension}`
+    Jimp.read(tempUpload, (err, image) => {
+    if (err) throw err;
+    image.resize(250, 250).quality(60).write(`./public/avatars/${filename}`);
+  });
     const resultUpload = path.join(avatarsDir, filename)    
     await fs.rename(tempUpload, resultUpload)
     const avatarURL = path.join("avatars", filename)
